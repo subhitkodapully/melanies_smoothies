02d45b2368ea92f1586c33ac215b6c9effef8ac2
subhitkodapully <34358629+subhitkodapully@users.noreply.github.com>
2025-09-18 16:59:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ if ingredients_list:
     st.write("ingredients_string=" + ingredients_string)
 
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response)
+    st.text(smoothiefroot_response.json())
 
 
     my_insert_statement = """ insert into smoothies.public.orders(ingredients,name_on_order)
