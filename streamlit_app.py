4a25ab92792ac084a0119c71e727a6a63eb77688
subhitkodapully <34358629+subhitkodapully@users.noreply.github.com>
2025-09-18 09:33:48 -0400

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,62 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col 
+from snowflake.snowpark.functions import when_matched
+
+# Write directly to the app
+st.title(f" :cup_with_straw: Customize Your Smoothies :cup_with_straw:")
+st.write(
+    """ Choose the fruits you want in your Soomthie!    
+    """
+)
+
+
+
+# option = st.selectbox(
+#    "What is your favorite fruit",
+#    ("Banana", "Strawberries", "Peaches"),
+#    index=None, )
+
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on the smoothie will be", name_on_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+
+
+
+ingredients_list = st.multiselect(
+    'Choose upto 5 ingredients:'
+    ,my_dataframe
+    ,max_selections = 5
+)
+
+if ingredients_list:
+    st.write(ingredients_list)
+    st.text(ingredients_list)
+
+    ingredients_string = ''    
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+    
+    
+    st.write("ingredients_string=" + ingredients_string)
+
+    my_insert_statement = """ insert into smoothies.public.orders(ingredients,name_on_order)
+    values ('""" + ingredients_string + """','""" +name_on_order+ """') """
+
+
+    st.write("my_insert_statement = " + my_insert_statement)
+
+    time_to_insert = st.button('Submit Button')
+
+    if time_to_insert:
+        session.sql(my_insert_statement).collect()
+        st.success('Your smoothie is ordered!',icon="✅")        
+
+#    if  ingredients_string:
+#        session.sql(my_insert_statement).collect()   
+#        st.success('Your smoothie is ordered!', '""" +name_on_order+ """' ,icon="✅")
