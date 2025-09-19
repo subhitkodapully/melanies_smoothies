bb30ee5d24a9171860a78d8b364b4aacf834efb3
subhitkodapully <34358629+subhitkodapully@users.noreply.github.com>
2025-09-19 14:06:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,12 @@ pd_df = my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.stop()
 
+ingredients_list = st.multiselect(
+    'Choose upto 5 ingredients:'
+    ,my_dataframe
+    ,max_selections = 5
+)
+
 
 # option = st.selectbox(
 #    "What is your favorite fruit",
@@ -37,11 +43,7 @@ pd_df = my_dataframe.to_pandas()
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on the smoothie will be", name_on_order)
 
-ingredients_list = st.multiselect(
-    'Choose upto 5 ingredients:'
-    ,my_dataframe
-    ,max_selections = 5
-)
+
 
 if ingredients_list:
     ingredients_string = ''
