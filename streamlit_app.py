acd670040c5c037d098f95937a02462469857aec
subhitkodapully <34358629+subhitkodapully@users.noreply.github.com>
2025-09-19 13:36:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, width=True)
 
 # Write directly to the app
