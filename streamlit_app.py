058e04f98b3885cc0a54bff1c091b95c0bbe9698
subhitkodapully <34358629+subhitkodapully@users.noreply.github.com>
2025-09-19 11:28:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ st.write(
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 
 # option = st.selectbox(
