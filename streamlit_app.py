911a236ddeba98a47dc83f8e72a813e23ca1d07b
subhitkodapully <34358629+subhitkodapully@users.noreply.github.com>
2025-09-20 08:31:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col 
-from snowflake.snowpark.functions import col, when_matched
 import pandas as pd
 import requests
 
